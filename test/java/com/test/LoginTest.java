f80deb90b645efd11653814c11e83f21a6693aad
Tushar Badgujar <109934104+TusharBadgujar02@users.noreply.github.com>
2023-03-02 09:53:31 +0530

    Keyword Driven Framework
    Facebook Page Automation with Keyword Driven Framework

---
+++ test/java/com/test/LoginTest.java
@@ -0,0 +1,17 @@
+package com.test;
+import org.testng.annotations.Test;
+
+import com.keywordengine.KeywordEngine;
+
+public class LoginTest {
+	
+	
+		
+		public KeywordEngine keywordEngine;
+			@Test
+			public void loginTest() {
+				 keywordEngine = new KeywordEngine();
+				 keywordEngine.startExecution("Sheet1");
+			}
+
+		}
